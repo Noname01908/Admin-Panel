47caa6a748f83d34567b3c0887ceff0087e951fb
Noname01908 <adeferdi19@gmail.com>
2024-12-10 20:18:21 +0700

    Setup API Google untuk Login With Google Account dan setup UI untuk Dashboard admin panel beserta Sidebarnya

--- src/app/AdminPanel/layout.tsx
+++ src/app/AdminPanel/layout.tsx
@@ -1,19 +1,29 @@
 "use client"
+import Loader from '@/Components/Admin-panel/Loader'
 import Login from '@/Components/Admin-panel/Login'
 import { useAppSelector } from '@/redux/hook'
 import { useSession } from 'next-auth/react'
 import React from 'react'
+import Sidebar from '@/Components/Admin-panel/Sidebar'
 
-const Layout = () => {
+const Layout = ({children}: {children: React.ReactNode}) => {
   const isLoading = useAppSelector (store => store.loadingReducer)
   const {data: session} = useSession()
   //
-  if(!session?.user)
+  if(!session?.user){
     return <Login/>
+  }
 
   return (
-    <div>
-
+    <div className='flex'>
+      <Sidebar/>
+      <div className='w-full h-full'>
+        {/* <Navbar/> */}
+        <div className='bg-gray-200 p-4 h-[calc(100vh-64px)]'>
+            {children}
+        </div>
+      </div>
+      {isLoading && <Loader/>}
     </div>
   )
 }
