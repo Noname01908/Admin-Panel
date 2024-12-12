2afd099c4cddd9171239d4bb5d9dea954f4060d3
Noname01908 <adeferdi19@gmail.com>
2024-12-12 21:39:42 +0700

    Setup Dashboard agar dapat mengambil data product dari database dengan API yang ditangkap oleh Axios

--- src/app/AdminPanel/Dashboard/page.tsx
+++ src/app/AdminPanel/Dashboard/page.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+"use client"
+import { setLoading } from '@/redux/features/loadingReducer'
+import { useAppDispatch } from '@/redux/hook'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 //import Layout from '../layout'
+export interface IProduct{
+  id: string
+  title: string
+  img: string
+  price: number
+  quantity: number
+}
 
 const Dashboard = () => {
+  const [products, setProducts] = useState([])
+  const [openpopup, setOpenPopUp] = useState(false)
+  const [updateTable, setUpdateTable] = useState(false)
+  //
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(setLoading(true))
+    //
+    axios.get("/api/get_products").then((res) => setProducts(res.data)).
+    catch(err => console.log(err)).finally(() => dispatch(setLoading(false)))
+  }, [updateTable])
+  //
   return (
     <div>Dashboard</div>
   )
